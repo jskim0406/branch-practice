5a5a65da4194e91d6aa8bf53e3546be8bbc0cc4e
jskim0406 <kjscop@gmail.com>
2020-04-22 15:18:31 +0900

    feat: change function to print 'buz' when 5 times number

--- hello.py
+++ hello.py
@@ -1,7 +1,10 @@
 for i in range(100):
-    if i%3 == 0:
-        print ('fiz')
+    if i%3 == 0 and i%5 !=0:
+        print('fiz')
+    elif i%5 == 0 and i%3 != 0:
+        print('buz')
     else:
         print(i)
+
 
 
